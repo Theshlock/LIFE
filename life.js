054bf99ea9c5e0f56f7500f7ea1400975a61a34b
Samuel Lockton <lockton.sam@gmail.com>
2024-04-03 10:25:48 +1100

    return level(xp)

--- life.js
+++ life.js
@@ -704,36 +704,4 @@ function victory() {
 menu()
 
 
-
-
-$('.ok').click(function() {
-	$('.progress').animate({
-	  width: "152px"
-	}, 500, function() {
-	  $('.target-lvl').addClass('lvl-animate');
-	  $('#initial-lvl').fadeOut('500');
-	  $('.line').fadeOut('500');
-	});
-	$('.cur-xp').each(function() {
-	  $(this).prop('Counter', 56).animate({
-		Counter: 108
-	  }, {
-		duration: 300,
-		easing: 'swing',
-		step: function(now) {
-		  $(this).text(Math.ceil(now));
-		}
-	  });
-	});
-  });
-  
-  $('.reset').click(function() {
-	$('.progress').animate({
-	  width: "114px"
-	}, 500, function() {
-	  $('.target-lvl').removeClass('lvl-animate');
-	  $('#initial-lvl').fadeIn('500');
-	  $('.line').fadeIn('500');
-	  $('.cur-xp').html("1500");
-	});
-  });
+function level(xp) {return xp**0.2}
