a48b92846617497c9dfe0f284d79f7640514cd8e
Samuel Lockton <lockton.sam@gmail.com>
2024-04-06 13:30:43 +1100

    speed up again

--- life.js
+++ life.js
@@ -647,7 +647,7 @@ function gameloop() {
 		contextM.fillText("△: " + Math.round(multiplier) ,360,550);
 		contextM.fillText(level + "/7",620,550);
 		contextM.fillText("+" + Math.round(bonus*1000)/1000,100,500);
-		zoom *= 1 + 0.005 * multiplier;
+		zoom *= 1 + 0.01 * multiplier;
 		time = Date.now();
 		screenX = Math.round(-xnorm * zoom + canvasWidth/2);
 		screenY = Math.round(-ynorm * zoom + canvasHeight/2);
