6112c397df80bd217afc074103c8f98d2eafabde
Samuel Lockton <lockton.sam@gmail.com>
2024-04-27 08:18:56 +0200

    debug

--- life.js
+++ life.js
@@ -619,7 +619,7 @@ function gameloop() {
 			xnorm += ( xRate / zoom ) * ( Date.now() - time)  / 10;
 			ynorm += ( yRate / zoom ) * ( Date.now() - time ) / 10;
 			multiplier = -0.5 - Math.log2(((((xnorm - portalX)*zoom)/1600)**2 + ((ynorm-portalY)*zoom/1200)**2)**0.5);
-			zoom *= (1 + 0.01 * multiplier);
+			zoom *= (1 + 0.01);
 			contextM.fillText(Date.now()-startTime,100,550);
 			contextM.fillText("△: " + Math.round(multiplier*1000)/1000 ,360,550);
 			contextM.fillText(level + "/7",620,550);
