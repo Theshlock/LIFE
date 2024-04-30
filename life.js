c4e6d3454c6a5cf81bdb9ea557a36e0519b9c2e7
Samuel Lockton <lockton.sam@gmail.com>
2024-04-30 11:40:33 +0200

    help text

--- life.js
+++ life.js
@@ -797,3 +797,11 @@ function downgrade(x) {
 function cycleSkins() {
 	console.log("this function is under construction")
 }
+
+function help() {
+	console.log("life is a game designed to test your accuracy and reflexes")
+	console.log("speed is calculated from how close the green portal is to the centre of the screen")
+	console.log("time attack consists of 7 levels designed to benchmark your ability")
+	console.log("zen mode infinitely generates random portal locations")
+	console.log("have fun")
+}
