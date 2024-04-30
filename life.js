bb950e628e42bb65fab9fdfd42d88c0c7c2d8e16
Samuel Lockton <lockton.sam@gmail.com>
2024-04-30 15:45:42 +0200

    dialog box

--- life.js
+++ life.js
@@ -805,3 +805,17 @@ function help() {
 	console.log("zen mode infinitely generates random portal locations")
 	console.log("have fun")
 }
+
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+// "Close" button closes the dialog
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
