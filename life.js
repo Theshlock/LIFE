8d9d2a87d7f507f15bff43e1910f3a30eb34d135
Samuel Lockton <lockton.sam@gmail.com>
2024-04-30 19:21:34 +0200

    revert change

--- life.js
+++ life.js
@@ -595,10 +595,7 @@ function gameloop() {
 		}
 	} else if (gamestate == "time attack") {
 		if (level == 8) {
-			if (rendered == 0) {
-				finalGameTime = Date.now()-startTime;
-				rendered = 1
-			}
+			finalGameTime = Date.now()-startTime;
 			contextM.fillText("You win",300,200);
 			contextM.fillText("final time: " + finalGameTime ,300,260);
 			localStorage.setItem("xp", Number(window.localStorage.xp) + bonus);
